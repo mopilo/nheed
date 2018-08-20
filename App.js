93ecb6cc75d54a24ed91029c16450e73853bebcd
mopilo <mopahemma@gmail.com>
2018-08-20 12:24:01 +0100

    persitGate

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Provider} from 'react-redux'
 import {Navigator} from './component/Navigator/AppNavigation'
-import store from './store/store'
+import {store, persistor} from './store/store'
 import { PersistGate } from "redux-persist/es/integration/react";
 
 
@@ -10,9 +10,9 @@ export default class App extends Component{
   render(){
     return(
       <Provider store ={store}>
-      <PersistGate>
-        
-      </PersistGate>
+        <PersistGate loading={null} persistor={persistor}>
+          <Navigator/>
+        </PersistGate>
       </Provider>
     )
   }
