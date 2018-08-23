a1c84a5f6e822142895008c2a21faf480a28cd99
mopilo <mopahemma@gmail.com>
2018-08-23 11:15:47 +0100

    getState

--- store/Actions/auth.js
+++ store/Actions/auth.js
@@ -1,13 +1,13 @@
 import {uiStartLoading, uiStopLoading} from './ui'
-import {login} from './actionType'
-import {REQUEST_URL, LOGIN_URL} from '../../component/Utility/local'
+import {login, register} from './actionType'
+import {REQUEST_URL, LOGIN_URL, SIGN_UP, OTP} from '../../component/Utility/local'
 import{ToastAndroid, AsyncStorage} from 'react-native'
 import { NavigationActions } from 'react-navigation'
 // import setAsync from '../../component/Utility/setAsync';
 
-
+//login
 export const loginUser = (userDetails) => (dispatch, getState) => {
-    const {isConnected} = getState();
+    const {isConnected} = getState().isConnected
     const signinURL = REQUEST_URL + LOGIN_URL;
 
     if (userDetails.email.trim() === '' || userDetails.password.trim() === '') {
@@ -87,38 +87,187 @@ export const authSetTokenUserId= (token, userId)=>{
     };
 }
 
-// export const authGetToken = () => (dispatch, getState)=> {
-//     const Promise = new Promise ((resolve, reject) => {
-//         const token = getState().auth.token;
-//         const userId = getState().aut.token 
-//         if(!token){
-//             AsyncStorage.getItem('token')
-//             .catch(err=>reject())
-//             .then(token => {
-//                 if (!token) {
-//                   throw new Error();
-//                 } else {
-//                   return token;
-//                 }
-//               });
-//             // resolve(token)
-//         }
-//     })
-// }
+//Register
 
-// export const authAutoSignIn = () => dispatch=>{
-//     const token = getState().auth.token;
-//     if(!token){
-//         AsyncStorage.getItem('token')
-//         .catch(err=>reject())
-//         .then(token => {
-//             if (!token) {
-//               throw new Error();
-//             } else {
-//                 dispatch(NavigationActions.navigate({ routeName: 'SignUp' }))
-//                 return token;
-//             }
-//           });
-//         // resolve(token)
-//     }
-// }
+export const registerUser = (userDetails) => (dispatch, getState) => {
+    const {isConnected} = getState();
+    const signinURL = REQUEST_URL + SIGN_UP;
+
+    if (userDetails.email.trim() === '' || userDetails.password.trim() === '' || userDetails.confirm.trim() === '') {
+        ToastAndroid.showWithGravity(
+            "Field can't be empty",
+            ToastAndroid.SHORT,               
+            ToastAndroid.CENTER
+        );
+    }
+    if (userDetails.password != userDetails.confirm){
+        ToastAndroid.showWithGravity(
+            "password mismatch",
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER
+        );
+    }
+
+    if(userDetails.email && userDetails.password){
+        if(!isConnected){
+            ToastAndroid.showWithGravity(
+                "Device offline",
+                ToastAndroid.SHORT,
+                ToastAndroid.CENTER
+            );
+        }
+        
+        else{
+            dispatch(uiStartLoading());
+
+            fetch(signinURL, {
+                method: "POST",
+                body: JSON.stringify({
+                    user: {
+                        email: userDetails.email,
+                        password: userDetails.password
+                    }
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                  'Accept': 'application/json'
+    
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(uiStopLoading());
+            })
+            .then(res => {return res.json()})
+            .then(resData => {
+                dispatch(uiStopLoading());
+                if (resData.error) {
+                    ToastAndroid.showWithGravity
+                    (resData.error, ToastAndroid.SHORT, ToastAndroid.CENTER);
+                }
+                else if(resData.message){
+                    ToastAndroid.showWithGravity(resData.message, ToastAndroid.SHORT, ToastAndroid.CENTER);
+                }
+                else {
+                    ToastAndroid.showWithGravity(
+                        'Authenticating',
+                        ToastAndroid.SHORT,
+                        ToastAndroid.CENTER
+                    )
+                    dispatch(
+                        setEmail(
+                            resData.data.email
+                        )
+                    );
+                    dispatch(NavigationActions.navigate({ routeName: 'OTP' }))
+                }
+            });
+        
+        }
+    }  
+}
+
+export const setEmail = (email) => dispatch=>{
+    //dispatch to store
+    dispatch(authSetEmailUser(email));
+    AsyncStorage.setItem('email', email);
+}
+
+export const authSetEmailUser= (email)=>{
+    return{
+        type: register,
+        email: email
+    };
+}
+
+//otp
+
+export const registerOTP = (userDetails) => (dispatch, getState) => {
+    const {isConnected} = getState();
+    const signinURL = REQUEST_URL + OTP;
+
+    if (userDetails.otp.trim() === '' ) {
+        ToastAndroid.showWithGravity(
+            "Field can't be empty",
+            ToastAndroid.SHORT,               
+            ToastAndroid.CENTER
+        );
+    }
+    if (userDetails.otp < 5){
+        ToastAndroid.showWithGravity(
+            "Incomplete",
+            ToastAndroid.SHORT,
+            ToastAndroid.CENTER
+        );
+    }
+
+    if(userDetails.otp){
+        if(!isConnected){
+            ToastAndroid.showWithGravity(
+                "Device offline",
+                ToastAndroid.SHORT,
+                ToastAndroid.CENTER
+            );
+        }
+        
+        else{
+            dispatch(uiStartLoading());
+
+            fetch(signinURL, {
+                method: "POST",
+                body: JSON.stringify({
+                    user: {
+                        email: userDetails.email,
+                        otp: userDetails.otp
+                    }
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                  'Accept': 'application/json'
+    
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(uiStopLoading());
+            })
+            .then(res => {return res.json()})
+            .then(resData => {
+                dispatch(uiStopLoading());
+                if (resData.error) {
+                    ToastAndroid.showWithGravity
+                    (resData.error, ToastAndroid.SHORT, ToastAndroid.CENTER);
+                }
+                else if(resData.message){
+                    ToastAndroid.showWithGravity(resData.message, ToastAndroid.SHORT, ToastAndroid.CENTER);
+                }
+                else {
+                    ToastAndroid.showWithGravity(
+                        'Authenticating',
+                        ToastAndroid.SHORT,
+                        ToastAndroid.CENTER
+                    )
+                    dispatch(
+                        setAsync(
+                            resData.token,
+                            resData.data.id.toString()                        )
+                    );
+                    dispatch(NavigationActions.navigate({ routeName: 'EditProfile' }))
+                }
+            });
+        
+        }
+    }  
+}
+
+
+export const authAutoSignIn = () => (dispatch, getState)=>{
+    const {token} = getState().AuthReducer
+    if(token){
+        dispatch(NavigationActions.navigate({ routeName: 'Home' }))
+    }
+    else{
+        dispatch(NavigationActions.navigate({ routeName: 'SingIn' }))
+    }
+}
+
