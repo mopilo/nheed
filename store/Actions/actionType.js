1c6e62ed38921f0ecd0ebee41bf68904a121a279
mopilo <mopahemma@gmail.com>
2018-08-23 11:15:19 +0100

    home action

--- store/Actions/actionType.js
+++ store/Actions/actionType.js
@@ -3,9 +3,13 @@ const register = 'SIGNUP'
 const otp = 'OTP'
 
 
+
 export const UI_START_LOADING = "UI_START_LOADING";
 export const UI_STOP_LOADING = "UI_STOP_LOADING";
 
 export const CHANGE_CONNECTION_STATUS = "CHANGE_CONNECTION_STATUS";
+
+export const HOME_SUCCESS = 'HOME_SUCCESS';
+export const ERROR = 'ERROR';
 
 export{login, register, otp};
