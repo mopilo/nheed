a9211c1dd7c3c6648100041241128489e63aafc4
mopilo <mopahemma@gmail.com>
2018-08-29 11:46:54 +0100

    new screens

---
+++ src/tabs/Upload.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+class Upload extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    Upload
+                </Text>
+            </View>
+        )
+    }
+}
+
+export default Upload
