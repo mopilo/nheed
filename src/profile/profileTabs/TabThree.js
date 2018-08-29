73b5a185dae5fe15715caa9e046fef01adc4cd72
mopilo <mopahemma@gmail.com>
2018-08-29 10:27:00 +0100

    new screen for tabs

---
+++ src/profile/profileTabs/TabThree.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native'
+
+export default class TabThree extends Component{
+
+    render(){
+        return(
+            <View>
+                <Text>
+                    three
+                </Text>
+            </View>
+        )
+    }
+}
