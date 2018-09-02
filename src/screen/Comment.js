b848a544b6af0be648dd6345ee4cc21fa935c74b
mopilo <mopahemma@gmail.com>
2018-09-02 09:26:30 +0100

    new screen

---
+++ src/screen/Comment.js
@@ -0,0 +1,213 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, TouchableOpacity,
+    ScrollView, TextInput, RefreshControl, Image, FlatList, ToastAndroid} from 'react-native';
+import { REQUEST_URL, HOME_URL } from '../Utility/local';
+import {Thumbnail} from 'native-base'
+
+
+class Comments extends Component{
+
+    state = {
+        text: '',
+        refreshing: true,
+        comments: [],
+    }
+
+    componentDidMount(){
+        this.fetchComments()
+        const profilePic = this.props.navigation.state.params.profilePic;
+    }
+
+    onRefresh = () => this.fetchComments();
+
+    // FETCH COMMENTS
+
+    fetchComments = () => {
+        let token = this.props.navigation.state.params.token;
+        let userId = this.props.navigation.state.params.userId;
+        let photoID = this.props.navigation.state.params.post;
+        const url = REQUEST_URL + HOME_URL + userId + '/posts/' + photoID;
+        // const url = REQUEST_URL + HOME_URL + userId + '/' + photoID + '/comments/'
+        this.setState({ refreshing: true})
+        fetch(url, {
+            headers: {
+                'content-type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        })
+        .then(res => { return res.json() })
+        .then(response => {
+            this.setState({
+                refreshing: false,
+                comments: response.comments
+            })
+            console.log(this.state.comments)
+            return response
+        })
+        .catch((err)=> console.log(err))
+    }
+
+    // POST COMMENTS 
+    postComment = () => {
+        let token = this.props.navigation.state.params.token;
+        let userId = this.props.navigation.state.params.userId;
+        let photoID = this.props.navigation.state.params.post;
+
+        const url = REQUEST_URL + HOME_URL + userId + '/' + photoID + '/comments/'
+        this._scrollView.scrollTo({ y: 0 });
+        if (this.state.text.trim() === '') {
+            ToastAndroid.showWithGravity(
+                "Field can't be empty",
+                ToastAndroid.SHORT,
+                ToastAndroid.CENTER
+            );
+        } 
+        else{
+            fetch(url,{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({
+                    comment: {
+                        post_id: photoID,
+                        account_id: userId,
+                        comment: this.state.text,
+                    }
+                })
+            })
+            .then((res)=> {return res.json()})
+            .then((response)=> {
+                this.setState({
+                    data: response.data,
+                    text: ''
+                })
+                this.fetchComments();
+                console.log(this.state.data, " post data")
+                return response;
+            })
+        }
+        
+    }
+
+    renderItem = ({item}) => {
+        return(
+            <View>
+                <View style={{flexDirection:'row', justifyContent: 'space-between',alignItems: 'center', padding: 5}}>
+                <View style={{flex: 0.8}}>
+                    <Thumbnail square source={{uri: item.account.profile_picture}} style={{ height: 35, width: 35 }}/>
+                </View>
+                <View style={{flex: 6, flexDirection: 'column', alignItems: 'flex-start' }}>
+                    <Text style={{fontFamily: 'Lato-Bold', fontSize: 12, color: '#000'}}>{item.account.name}</Text>
+                    <Text style={{fontFamily: 'Lato-Light', fontSize: 12}}>time</Text>
+                </View>
+
+            </View>
+                <Text style={{fontFamily: 'Lato-Regular', fontSize: 14, padding: 5, marginBottom: 10, color:'#000'}}>{item.comment}</Text>
+            </View>
+        )
+    }
+
+    render(){
+        const profilePic = this.props.navigation.state.params.profile_picture;
+        const caption = this.props.navigation.state.params.caption
+        return(
+            <View style={styles.container}>
+                <View style={styles.body}>
+                {/* SCROLL LIST */}
+                    <ScrollView
+                        ref={(scrollView) => {this._scrollView = scrollView;}}
+                        refreshControl = {
+                            <RefreshControl
+                                refreshing={this.state.refreshing}
+                                onRefresh={this.onRefresh}
+                            />
+                        }
+
+                    >
+                    {
+                        caption ?
+                        <View style={{flexDirection: 'row', borderBottomColor: 'grey', borderBottomWidth: 0.5, marginBottom: 5}}>
+                            <Thumbnail square source={{uri: profilePic}} style={{ height: 30, width: 30, borderRadius: 5 }}/>
+                            <Text style={{fontSize: 14, fontFamily: 'Lato-Regular', margin: 10, alignSelf: 'flex-start' }}>
+                                {caption}
+                            </Text>
+                        </View> : null
+                    }
+                    
+                    <FlatList
+                        data={this.state.comments}
+                        renderItem={this.renderItem}
+                        keyExtractor={(item, index) => item.id.toString()}
+                    />
+                    </ScrollView>
+                </View>
+                <View style={styles.tabBar}>
+                    <View style={{flex: 1}}>
+                        <View style={{width: 35, height: 35, justifyContent: 'center', alignItems: 'center'}}>
+                            <Thumbnail square source={{uri: profilePic}} style={{ height: 30, width: 30, borderRadius: 5 }}/>
+                        </View>
+                    </View>
+                    <View style={{flex: 6}}>
+                        <TextInput
+                            placeholder= 'Add a comment...'
+                            placeholderTextColor='#f2f2f2'
+                            underlineColorAndroid = 'transparent'
+                            style={{fontFamily: 'Lato-Regular', fontSize: 14, color: '#f2f2f2'}}
+                            onChangeText={(text) => this.setState({text})}
+                            value={this.state.text}
+                            maxLength={150}
+                            multiline = {true}
+                            numberOfLines = {10}
+                        />
+                    </View>
+
+                    <View style={{flex: 1}}>
+                        <TouchableOpacity onPress={this.postComment} >
+                            <Text style={{color: '#f2f2f2', fontFamily: 'Lato-Regular', fontSize: 16, margin: 5}}>Post</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    body: {
+        flex: 1
+    },
+    tabBar: {
+        backgroundColor: '#251b33',
+        height: 60,
+        borderTopWidth: 0.5,
+        borderColor: '#E5E5E5',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        padding: 10
+    }
+})
+
+
+const mapStateToProps = state => {
+    return {
+        pic: state.homeReducer.pic,
+        token: state.authReducer.token,
+        userId: state.authReducer.userId
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        network: (status) => dispatch(isConnected(status)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Comment)
